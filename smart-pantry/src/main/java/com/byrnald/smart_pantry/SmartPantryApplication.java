13c42cb83f2ceb4ba576f70f75b1a731c74b9ecd
byrnald <byrnald@gmail.com>
2026-01-05 14:35:41 -0500

    Initial Spring Boot setup

---
+++ smart-pantry/src/main/java/com/byrnald/smart_pantry/SmartPantryApplication.java
@@ -0,0 +1,13 @@
+package com.byrnald.smart_pantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPantryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPantryApplication.class, args);
+	}
+
+}
---
+++ smart-pantry/src/test/java/com/byrnald/smart_pantry/SmartPantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byrnald.smart_pantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

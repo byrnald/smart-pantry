46421142f87a0923ef617022709ed5b94ea44e51
byrnald <byrnald@gmail.com>
2026-01-06 16:31:10 -0500

    Feature: Full CRUD and Business Logic implemented and tested, Specifically updateItems, and deleteItems, which are U/D by their ID.

--- smart-pantry/src/main/java/com/byrnald/smart_pantry/service/PantryService.java
+++ smart-pantry/src/main/java/com/byrnald/smart_pantry/service/PantryService.java
@@ -1,5 +1,77 @@
 package com.byrnald.smart_pantry.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.byrnald.smart_pantry.model.PantryItem;
+import com.byrnald.smart_pantry.repository.PantryRepository;
+
+@Service // this just contains the logic for our application, it acts as the middle man for our controller.
+// The controller will call the service, and the service will call the repository to interact with the database
+// when we setup service we create an instance of this class.
 public class PantryService {
+    /// we need to inject the PantryRepository into this service so that we
+    //  can use it to interact with the database. We do this through constructor injection, 
+    // which is a common pattern in spring boot applications. This allows us to easily 
+    // test our service by mocking the repository if needed.
+    private final PantryRepository pantryRepository;
+
+    public PantryService(PantryRepository pantryRepository) {
+        this.pantryRepository = pantryRepository;
+    }
+
+    public List<PantryItem> getAllItems() {
+        return pantryRepository.findAll();
+    }
+
+    public PantryItem addItem(PantryItem item) {
+        return pantryRepository.save(item);
+    }
+
+    public List<PantryItem> getExpiringSoon() {
+        LocalDate today = LocalDate.now();
+        LocalDate threeDaysFromNow = today.plusDays(3);
+        //this method calls the findByExpirationDateBetween method in the PantryRepository 
+        /// which retrieves all pantry items that have an expiration date between today and three days from now.
+        return pantryRepository.findByExpirationDateBetween(today, threeDaysFromNow);
+    }
+    //so after all of this
+    /// we have a repository, service, and soon the controller.
+    
+    //now we need to add the deleteItem method from our controller to this service, so that
+    //we can actually delete the items from our pantry
+    
+    /*
+    public void deleteItem(Long id) { 
+        for (int i = 0; i < pantryRepository.findAll().size(); i++) {
+            if (pantryRepository.findAll().get(i).getId() == id) { 
+                pantryRepository.deleteById(id);
+            } else { 
+                System.out.println("Item with id " + id + " not found.");
+            }
+        }
+    }
+    */ //this is my old deletion method, but it was not efficient as it just loops/iterates through all of the items
+    //until it finds the item with the ID.
+
+    public void deleteItem(Long id) { 
+        if (pantryRepository.existsById(id)) { 
+            pantryRepository.deleteById(id);
+        } else { 
+            //we could throw an exception here but for now we will just print out a message
+            System.out.println("Item with id " + id + " not found.");
+        }
+    }
+
+    public PantryItem updateItem(Long id, PantryItem newItemData) { 
+        return pantryRepository.findById(id).map(item -> {
+            item.setName(newItemData.getName());
+            item.setQuantity(newItemData.getQuantity());
+            item.setExpirationDate(newItemData.getExpirationDate());
+            return pantryRepository.save(item);
+        }).orElseThrow(() -> new RuntimeException("Item with id: " + id));
+    }
 
 }

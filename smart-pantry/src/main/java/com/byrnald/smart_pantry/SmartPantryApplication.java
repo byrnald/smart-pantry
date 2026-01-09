2c355a6e91314e6bb147c34379da6f698b5f9ed9
byrnald <byrnald@gmail.com>
2026-01-08 21:19:13 -0500

    added a new feature: added data seeding that contains my personal homelab items that i'll need in the future. Also egg, i eat a lot of eggs.

--- smart-pantry/src/main/java/com/byrnald/smart_pantry/SmartPantryApplication.java
+++ smart-pantry/src/main/java/com/byrnald/smart_pantry/SmartPantryApplication.java
@@ -9,5 +9,9 @@ public class SmartPantryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmartPantryApplication.class, args);
 	}
+	// this is just the main class, runs on the spring boot server and starts the application
+	//to look at the database/seeded data you can go to http://localhost:8080/h2-console and then use the following settings:
+	// JDBC URL: jdbc:h2:mem:testdb (this has to be the same as the one in application.properties)
+	// or to see its json format you can go to http://localhost:8080/api/pantry and it will show you all the items
 
 }
